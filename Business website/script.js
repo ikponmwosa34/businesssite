8861556e3accb5a8a0ac0cc0fcdd5b00dcc56399
ikponmwosa34 <118065715+ikponmwosa34@users.noreply.github.com>
2024-09-20 18:23:50 +0100

    Add files via upload
    First commit

---
+++ Business website/script.js
@@ -0,0 +1,6 @@
+const toggleButton = document.getElementById('toggleButton');
+const navLinks = document.getElementById('navLinks');
+
+toggleButton.addEventListener('click', () => {
+    navLinks.classList.toggle('active');
+});
